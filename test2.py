9390961a56d7c750518c83821b79058e8826d554
kvetinskiy <nikenike932@gmail.com>
2019-10-09 22:02:19 +0600

    v8

--- test2.py
+++ test2.py
@@ -7,18 +7,18 @@ keyboard1.row('Цена\U0001F4B5', 'Бизнес\U0001F4B0', 'Контакты\
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Здравствуйте! Выберите интересующую Вас позицию.', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Здравствуйте! \nВыберите интересующую Вас позицию\U0001F4CB', reply_markup=keyboard1)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
 
       if message.text == "Цена\U0001F4B5":
-          bot.send_message(message.chat.id, '\U0001F4DDСоздание бота - 10000р\n\U0001F9D4Администрирование - 1000р')
+          bot.send_message(message.chat.id, '\U0001F4DDСоздание бота - от 40000тг(8000р)\n\U0001F9D4Администрирование - от 10000 тг/мес(2000р/мес)')
 
       if message.text == "Бизнес\U0001F4B0":
           bot.send_message(message.chat.id, 'Боты широко используются государственными учреждениями и сетевыми компаниями\nОни существуют для автоматизации и облегчения работы с клиентами\nДля уточнения информации воспользуйтесь разделом "Контакты"')
 
       if message.text == "Контакты\U0000260E":
-          bot.send_message(message.chat.id, 'Наши контакты\n\U0001F4E7E-mail: n.kvetinskiy@gmail.com\n\U00002708Telegram: @kvetinskiy\n\U0001F4F1Мобильный телефон/WhatsApp: +7(705)288-11-22')
+          bot.send_message(message.chat.id, 'Наши контакты:\n\U0001F4E7E-mail: n.kvetinskiy@gmail.com\n\U00002708Telegram: @kvetinskiy\n\U0001F4F1Мобильный телефон/WhatsApp: +7(705)288-11-22')
 
 bot.polling()
